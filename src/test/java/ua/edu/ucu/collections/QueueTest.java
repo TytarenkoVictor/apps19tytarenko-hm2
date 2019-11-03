11c11a738d45804cb7d9574b54973277bfd1daa4
TytarenkoVictor <44294074+TytarenkoVictor@users.noreply.github.com>
2019-11-03 19:39:56 +0200

    Update QueueTest.java

--- src/test/java/ua/edu/ucu/collections/QueueTest.java
+++ src/test/java/ua/edu/ucu/collections/QueueTest.java
@@ -1,12 +1,37 @@
 package ua.edu.ucu.collections;
 
-import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
 public class QueueTest {
-    
-    @Test
-    public void testSomeMethod() {
+    private Queue q;
+
+    @Before
+    public void setUp() {
+        q = new Queue();
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3.0);
+        q.enqueue("d");
     }
-    
+
+    @Test
+    public void testPeek() {
+        assertEquals(1, q.dequeue());
+        assertEquals(2, q.dequeue());
+        assertEquals(3.0, q.dequeue());
+    }
+
+    @Test
+    public void testDequeue() {
+        assertEquals(1, q.peek());
+    }
+
+    @Test
+    public void testEnqueue(){
+        q.enqueue(22);
+        assertEquals(1, q.dequeue());
+    }
+
 }
