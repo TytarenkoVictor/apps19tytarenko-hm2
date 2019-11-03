208424d4990bb090db16498edd74574027ecb3a5
TytarenkoVictor <44294074+TytarenkoVictor@users.noreply.github.com>
2019-11-03 19:39:27 +0200

    Update StackTest.java

--- src/test/java/ua/edu/ucu/collections/StackTest.java
+++ src/test/java/ua/edu/ucu/collections/StackTest.java
@@ -1,12 +1,36 @@
 package ua.edu.ucu.collections;
 
-import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
 public class StackTest {
-    
-    @Test
-    public void testSomeMethod() {
+    private Stack s;
+
+    @Before
+    public void setUp() {
+        s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3.0);
+        s.push("d");
     }
-    
+
+    @Test
+    public void testPop() {
+        assertEquals("d", s.pop());
+        assertEquals(3.0, s.pop());
+        assertEquals(2, s.pop());
+    }
+
+    @Test
+    public void testPeek() {
+        assertEquals("d", s.peek());
+    }
+
+    @Test
+    public void testPush(){
+        s.push("RR");
+        assertEquals("RR", s.peek());
+    }
 }
