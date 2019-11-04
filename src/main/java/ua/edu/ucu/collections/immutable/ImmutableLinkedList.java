d0d7796453fae5a936f0b8fbb54287d49ca3ebf1
TytarenkoVictor <44294074+TytarenkoVictor@users.noreply.github.com>
2019-11-04 15:34:45 +0200

    Update ImmutableLinkedList.java

--- src/main/java/ua/edu/ucu/collections/immutable/ImmutableLinkedList.java
+++ src/main/java/ua/edu/ucu/collections/immutable/ImmutableLinkedList.java
@@ -6,6 +6,32 @@ public class ImmutableLinkedList implements ImmutableList{
     private Node head;
     private Node tail;
     private int size;
+    
+    public class Node {
+        private Node next;
+        private Object value;
+
+        public Node(Object value) {
+            this.value = value;
+            next = null;
+        }
+
+        public void setNext(Node newset) {
+            next = newset;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setValue(Object newval) {
+            value = newval;
+        }
+
+        public Object getValue() {
+            return value;
+        }
+    }
 
     public ImmutableLinkedList() {
         head = null;
