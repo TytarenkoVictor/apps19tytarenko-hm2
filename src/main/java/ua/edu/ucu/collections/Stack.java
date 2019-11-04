5f0589316b904ca343fa6ab75139d20542cea1a4
TytarenkoVictor <44294074+TytarenkoVictor@users.noreply.github.com>
2019-11-04 16:16:23 +0200

    Update Stack.java

--- src/main/java/ua/edu/ucu/collections/Stack.java
+++ src/main/java/ua/edu/ucu/collections/Stack.java
@@ -1,5 +1,25 @@
 package ua.edu.ucu.collections;
 
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
+
 public class Stack {
-    
+    private ImmutableLinkedList st;
+
+    public Stack() {
+        st = new ImmutableLinkedList();
+    }
+
+    public Object peek() {
+        return st.getFirst();
+    }
+
+    public void push(Object e) {
+        st = st.addFirst(e);
+    }
+
+    public Object pop() {
+        Object result = peek();
+        st = st.removeFirst();
+        return result;
+    }
 }
