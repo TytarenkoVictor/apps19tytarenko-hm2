ff2ad9440f5db69bb324d936eefa5962295ee89f
TytarenkoVictor <44294074+TytarenkoVictor@users.noreply.github.com>
2019-11-04 16:16:48 +0200

    Update Queue.java

--- src/main/java/ua/edu/ucu/collections/Queue.java
+++ src/main/java/ua/edu/ucu/collections/Queue.java
@@ -1,5 +1,25 @@
 package ua.edu.ucu.collections;
 
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
+
 public class Queue {
-    
+    private ImmutableLinkedList q;
+
+    public Queue() {
+        q = new ImmutableLinkedList();
+    }
+
+    public Object peek() {
+        return q.getFirst();
+    }
+
+    public Object dequeue() {
+        Object dec = peek();
+        q = q.removeFirst();
+        return dec;
+    }
+
+    public void enqueue(Object e) {
+        q = q.addLast(e);
+    }
 }
